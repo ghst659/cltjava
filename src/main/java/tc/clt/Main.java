69db0242b68b73b72fe49b3a14d5dafe6d35c4a4
TSC <gst659@gmail.com>
2019-05-05 07:36:38 -0700

    Cleanup.

--- src/main/java/tc/clt/Main.java
+++ src/main/java/tc/clt/Main.java
@@ -10,42 +10,44 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class Main {
-    public final static String appName = "Command";
+    public final static String appName = Main.class.getName();
     public final static String appDesc = String.join("\n",
-        "This is a template command-line tool.",
-        "This is the docstring explaining the command.");
+            "This is a template command-line tool.",
+            "This is the docstring explaining the command.");
+
     public static void main(String[] argv) {
-        int exitCode = 0;
-        ArgumentParser parser = ArgumentParsers.newArgumentParser(appName)
-                                    .description(appDesc);
+        ArgumentParser parser = ArgumentParsers.newFor(appName).build()
+                .defaultHelp(true)
+                .description(appDesc);
         parser.addArgument("-o", "--output").type(String.class).metavar("OUTPUT")
-            .dest("output").setDefault("/tmp")
-            .help("Output destination path.");
+                .dest("output").setDefault("/tmp")
+                .help("Output destination path.");
         parser.addArgument("-n", "--noop").action(Arguments.storeTrue())
-            .dest("noop")
-            .help("Do not execute, dry run only.");
+                .dest("noop")
+                .help("Do not execute, dry run only.");
         parser.addArgument("-v", "--verbose").action(Arguments.storeTrue())
-            .dest("verbose")
-            .help("Run verbosely.");
+                .dest("verbose")
+                .help("Run verbosely.");
         parser.addArgument("targets").nargs("*").metavar("TARGET")
-            .help("Targets for this command.");
+                .help("Targets for this command.");
+        Namespace res = null;
         try {
-            final Namespace res = parser.parseArgs(argv);
-            final boolean verbose = res.getBoolean("verbose");
-            final boolean noop = res.getBoolean("noop");
-            final String outPath = res.getString("output");
-            final List<String> targets = res.getList("targets");
-            if (verbose) {
-                System.err.printf("Output path: %s\n", outPath);
-            }
-            Consumer<String> op = noop ?
-                                      (s)->System.err.println("[noop]") :
-                                      (s)->System.err.println(s);
-            targets.forEach(op);
+            res = parser.parseArgs(argv);
         } catch (ArgumentParserException e) {
             parser.handleError(e);
-            exitCode = 1;
+            System.exit(1);
         }
-        System.exit(exitCode);
+        final boolean verbose = res.getBoolean("verbose");
+        final String outPath = res.getString("output");
+        if (verbose) {
+            System.err.printf("Output path: %s\n", outPath);
+        }
+        final List<String> targets = res.getList("targets");
+        final boolean noop = res.getBoolean("noop");
+        Consumer<String> op = noop ?
+                (s) -> System.err.println("[noop]") :
+                (s) -> System.err.println(s);
+        targets.forEach(op);
+        System.exit(0);
     }
 }
